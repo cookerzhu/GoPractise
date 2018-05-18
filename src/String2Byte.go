f5b5177448c4455e9ede8f7c522fad5da26d1293
zhuguangchuanwj62961 <zhuguangchuanwj62961@touna.cn>
2018-05-18 17:23:57 +0800

    commit

---
+++ src/String2Byte.go
@@ -0,0 +1,12 @@
+/**
+@See http://www.cnblogs.com/mushroom/p/8998538.html
+ */
+package main
+import "fmt"
+func main() {
+	s := []byte("")
+	s1 := append(s, 'a')
+	s2 := append(s, 'b')
+	//fmt.Println(s1, "==========", s2)
+	fmt.Println(string(s1), "==========", string(s2))
+}
--- src/channel.go
+++ src/channel.go
@@ -6,9 +6,9 @@ import (
 
 func main(){
 	ch := make(chan int)
+	v := <- ch  //先从channel取，就会deadlock，如果先往channel放（和下面一行代码交换，就ok）
 	go put(ch)
-	v := <- ch  //先从channel取，就会阻塞，如果先放（和下面一行代码交换，就ok）
-	fmt.Print(v)
+	fmt.Print("get value :" ,v)
 }
 
 func put(ch chan int){
