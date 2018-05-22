fb9323e0b28974f60d1e88e003f0de9870070df3
zhuguangchuanwj62961 <zhuguangchuanwj62961@touna.cn>
2018-05-22 15:23:55 +0800

    String2Bytes

--- src/String2Byte.go
+++ src/String2Byte.go
@@ -7,7 +7,7 @@ func main() {
 	s := []byte("")//cap is 32 ,len is 0 没有下面的注释，slice分配在栈上面，初始化大小是32的数组
 	s1 := append(s, 'a')
 	s2 := append(s, 'b')
-	//fmt.Println(s1, "==========", s2)   去掉注释，会发生逃（slice数组会分配到堆上，是空数组，s1,s2都是引用复制的新slice）
+	//fmt.Println(s1, "==========", s2)   去掉注释，会发生逃逸分配（slice数组会分配到堆上，是空数组，s1,s2都是引用复制的新slice）
 	fmt.Println(string(s1), "==========", string(s2))
 
 
